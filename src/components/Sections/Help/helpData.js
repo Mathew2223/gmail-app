35e4a0a3880e29646d27355ee2f0ad6158c6f2b8
Ситниковы <sitnikovmatvey2008@gmail.com>
2026-05-01 21:12:07 +0300

    add ai images for main page

--- src/components/Sections/Help/helpData.js
+++ src/components/Sections/Help/helpData.js
@@ -1,17 +1,21 @@
+import connection_image from '../../../assets/help_images/connection.png'
+import importantTime_image from '../../../assets/help_images/importantTime.png'
+import reminder_image from '../../../assets/help_images/reminder.png'
+
 export const helpData = [
     {
         title: "Всегда оставайтесь на связи",
         desc: "В окне Gmail можно переписываться в чате, подключаться к видеозвонкам и работать с документами.",
-        image: "https://lh3.googleusercontent.com/OJ4EfHYcL2U4vOUDwPYHdOWJIapcDvUsJpf6Pw6K4vJ-xaElcOopNNLqy5dc7a7rQZXU_jLXk_QqIBipqst2gpCIQT8EVhflIUfv7w=e365-pa-nu-rw-w527"
+        image: connection_image
     },
     {
         title: "Освободите ценное время",
         desc: "Умный ввод поможет писать письма и сообщения быстрее, а значит вы будете тратить меньше времени на переписку.",
-        image: "https://lh3.googleusercontent.com/bDOrDNxVpvL4z3bOEryIULP1OQW23C8mgv7eka42xcrSAP5Iu9hx_RDJZ7X_6NU1LFer7IU4MmOCeE2kNGVRC9uSrO7s_BehJAg=e365-pa-nu-rw-w527"
+        image: importantTime_image
     },
     {
         title: "Помните о том, что важно",
         desc: "Ненавязчивые напоминания помогут не упустить письма, требующие ответа.",
-        image: "https://lh3.googleusercontent.com/R5STxzbIpvScTy6zXwFT5KT716R579-24F0ZMHsNccGGOOphFjSrp7PCVhvwjua8-OwBATUy0KymKmYKpmxtvJWxY3H1V2hWkibbtA=e365-pa-nu-rw-w684"
+        image: reminder_image
     },
 ]
--- src/components/Sections/Security/securityData.js
+++ src/components/Sections/Security/securityData.js
@@ -1,22 +1,27 @@
+import add_image from '../../../assets/safe_images/add.png'
+import protected_image from '../../../assets/safe_images/protected.png'
+import phishing_image from '../../../assets/safe_images/phishing.png'
+import confidentiality_image from '../../../assets/safe_images/confidentiality.png'
+
 export const securityData = [
     {
         title: "Мы не используем содержимое почты для подбора рекламы",
         desc: "В Gmail все исходящие и входящие письма шифруются с использованием ведущих в отрасли технологий. Письма в Gmail никогда не используются для персонализации рекламы.",
-        image: "https://lh3.googleusercontent.com/BR_gXTPBQrwb4aTeTpBYAMSpl_xOZieCCxX_5Awuir3UOuPsWK1J-zdPZyIEZAxwLO6tnX_Ea_3wKAnqkEYn0Wj9zGnt6m4ZqAI=e365-pa-nu-rw-w684"
+        image: add_image
     },
     {
         title: "Каждый день Gmail защищает более миллиарда пользователей",
         desc: "Gmail блокирует 99,9 % писем со спамом, вредоносным ПО и подозрительными ссылками.",
-        image: "https://lh3.googleusercontent.com/HP1YfjeevslFsDg0wS4rzRNiNXkhLYhW1Sa_0MaFCEsopR1YxAjbegobuqAMscHd93ikHlKQBiTc_WlQWU9qmqp_Dx9qmBPfz6E=e365-pa-nu-rw-w684"
+        image: protected_image
     },
     {
         title: "Передовые технологии защищают от фишинга",
         desc: "Если вам придет подозрительное письмо, Gmail предупредит вас о возможных рисках.",
-        image: "https://lh3.googleusercontent.com/j73UcrFihOUZ-WL-asfh7KViJVGHmWyf8t8qC_PPEXUxpFvBo72oh5IgVUgqpJP7_tbu__rmcvI7QNOy40N7CEs2DraTv6Xl7cY=e365-pa-nu-rw-w684"
+        image: phishing_image
     },
     {
         title: "Конфиденциальность исходящих писем",
         desc: "Конфиденциальный режим позволяет задать срок доступа к письму, включить доступ по коду из SMS, запретить пересылку, копирование и печать письма, а также скачивание файлов.",
-        image: "https://lh3.googleusercontent.com/8cHO7WaYQsOObHs3PA0_7Au93UCIvvRzCoYA6XSZMd1W3TGBRQtJgroL6ruN_4TmIuyiCWqgwlhZz4IZaZfcVc3Q9o-Eykla3VLcOA=e365-pa-nu-rw-w684"
+        image: confidentiality_image
     }
 ]
